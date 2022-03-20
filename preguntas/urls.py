b0415edc59379e9ad8d77570c76b67355ea2cca7
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-20 17:48:20 -0600

    Proyecto 2

--- preguntas/urls.py
+++ preguntas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetalleRespuesta.as_view(), name='detalle-respuesta'),
     path('actualizar/<int:pk>/', views.ActualizarRespuesta.as_view(), name='actualizar-respuesta'),
     path('eliminar/<int:pk>/', views.EliminarRespuesta.as_view(), name='eliminar-respuesta'),
+    path('registro/', views.CrearUsuario.as_view(), name='agregar-usuario'),
 ]
--- preguntas/views.py
+++ preguntas/views.py
@@ -1,32 +1,43 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Choice
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-#TODO Obligar al usuario a registrarse antes de acceder a la aplicación
+from .forms import UserRegisterForm
 
 class CrearRespuesta(LoginRequiredMixin, CreateView):
     model = Choice
     fields = '__all__'
     template_name = 'agregar_respuesta.html'
 
+class CrearUsuario(SuccessMessageMixin, CreateView):
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('index')
+    template_name = 'agregar_usuario.html'
+    success_message = "Tu cuenta se ha creado con éxito"
+
 class DetalleRespuesta(LoginRequiredMixin, DetailView):
     model = Choice
     template_name = 'ver_respuesta.html'
+
 
 class ActualizarRespuesta(LoginRequiredMixin, UpdateView):
     model = Choice
     fields = ['choice_text']
     template_name = 'editar_respuesta.html'
 
+
 class EliminarRespuesta(LoginRequiredMixin, DeleteView):
     model = Choice
     template_name = 'eliminar_respuesta.html'
     success_url = reverse_lazy('index')
+
+
 class ListarRespuestas(LoginRequiredMixin, ListView):
     model = Choice
-    template_name ='listar_respuestas.html'
+    template_name = 'listar_respuestas.html'
+
     def get_queryset(self):
         return Choice.objects.filter(user=self.request.user)
